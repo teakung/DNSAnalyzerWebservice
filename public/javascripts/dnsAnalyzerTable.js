6b276a86891b8414cff80234bb16e02d876e9171
teakung <black.teakung@gmail.com>
2017-05-21 20:16:40 +0700

    merge all page and ton of changes

--- public/javascripts/dnsAnalyzerTable.js
+++ public/javascripts/dnsAnalyzerTable.js
@@ -1,5 +1,30 @@
+var tableGraphApiUrl = 'http://158.108.183.21:3000/dnsApi/topList2'
+//http://158.108.183.21:3000/dnsApi/topList2/1489597200/1489680000?qtype=countQuery
+var tableGraphStartTime = ''
+var tableGraphEndTime = ''
+var tableGraphQueryType = 'countQuery'
+
+$('#tableContainer').DataTable( {
+    "ajax": 'http://158.108.183.21:3000/dnsApi/topList2/1489597200/1489680000?qtype=countQuery'
+} );
+
 $(document).ready(function() {
-    $('#tableContainer').DataTable( {
-      "ajax": 'http://158.108.183.21:3000/dnsApi/topList/1489568400/1489572000?qtype=countQtype'
-    } );
+    $('input[name="tableDaterangePicker"]').daterangepicker(
+      {
+        locale: {
+          format: 'YYYY-MM-DD'
+        }
+      }, 
+      function(start, end, label) {
+        //alert("A new date range was chosen: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+        var queryStartDateIn = Date.parse(start.format('YYYY-MM-DD')+'T00:00:00+07:00');
+        var queryEndDateIn = Date.parse(end.format('YYYY-MM-DD')+'T00:00:00+07:00');
+        queryStartDateIn = queryStartDateIn/1000;
+        queryEndDateIn = queryEndDateIn/1000;
+        tableGraphStartTime = queryStartDateIn
+        tableGraphEndTime = queryEndDateIn
+        //alert(summaryGraphStartTime)
+        //alert(summaryGraphEndTime)
+      }
+    )
 } );
--- routes/dnsApi.js
+++ routes/dnsApi.js
@@ -18,7 +18,7 @@ router.get('/summary/:startTime/:endTime', function (req, res, next) {
   var days = 14
   var querySize = days*24
   client.search({
-  index: 'dnsanalyzerstore',
+  index: 'dnsanalyzerstore3',
   type: 'analyzedhour',
   body: {
   	"size": querySize,
@@ -37,12 +37,12 @@ router.get('/summary/:startTime/:endTime', function (req, res, next) {
 	}
 	}).then(function (resp) {
     var hits = resp.hits.hits;
-    var queryType = req.query.qtype;
+    var summaryQtype = req.query.qtype;
     var result = []
     for(var i = 0 ;i <= days-1 ; i++){
     	for(var j=0; j <= 23 ;j++){
     		if(hits[(i*24)+j] != undefined){
-    			result[(i*24)+j] = [parseInt(j),parseInt(i),hits[(i*24)+j]._source[queryType],(hits[(i*24)+j]._source.countNxdomain/hits[(i*24)+j]._source.countNoerror),hits[(i*24)+j]._source.timestamp_s]
+    			result[(i*24)+j] = [parseInt(j),parseInt(i),hits[(i*24)+j]._source[summaryQtype],(hits[(i*24)+j]._source.countNxdomain/hits[(i*24)+j]._source.countNoerror),hits[(i*24)+j]._source.timestamp_s]
     		}
     		else{
     			result[(i*24)+j] = [parseInt(j),parseInt(i),0]
@@ -62,10 +62,9 @@ router.get('/summary/:startTime/:endTime', function (req, res, next) {
 router.get('/timeseries/:startTime/:endTime', function (req, res, next) {
   console.log('startTime:', req.params.startTime);
   console.log('endTime:', req.params.endTime);
-  console.log('resType:', req.query.restype)
 
   client.search({
-  index: 'dnsanalyzerstore',
+  index: 'dnsanalyzerstore3',
   type: 'analyzedminute',
   body: {
     "size": 10000,
@@ -86,7 +85,6 @@ router.get('/timeseries/:startTime/:endTime', function (req, res, next) {
     var hits = resp.hits.hits;
     console.log(hits.length)
     var result = []
-    var resType = req.query.restype;
     for(var i = 0;i<hits.length;i++){
       result[i] = [parseInt(i),
       hits[i]._source.timestamp_s,
@@ -118,10 +116,6 @@ router.get('/timeseries/:startTime/:endTime', function (req, res, next) {
       hits[i]._source.countQtype.ANY,
       hits[i]._source.countQclass.IN]
 
-      
-/*      result[i] = [parseInt(i),
-      hits[i]._source.timestamp_s,
-      hits[i]._source[resType]]*/
     }
     //res.json(hits)
     res.json(result);
@@ -133,16 +127,158 @@ router.get('/timeseries/:startTime/:endTime', function (req, res, next) {
   //next()
 })
 
+function mergeJsonArray(a1,a2){
+  var res = a1
+  var nameExist = false
+
+  for (var i = 0; i < a2.length; i++){
+    nameExist = false
+    for(var j = 0;j < res.length; j++){
+      if (res[j].hostname == a2[i].hostname){
+        nameExist = true
+        res[j].count += a2[i].count;
+        break;
+      }
+    }
+    if(!nameExist){
+      res.push(a2[i])
+    }
+  }
+  return res
+}
+
 router.get('/topList/:startTime/:endTime', function (req, res, next) {
+  console.log('startTime:', req.params.startTime);
+  console.log('endTime:', req.params.endTime);
+  console.log('qtype:', req.query.qtype)
+  var topListQType = req.query.qtype;
+
+  client.search({
+  index: 'dnsanalyzerstore3',
+  type: 'analyzedhour',
+  body: {
+    "size": 1000,
+    "from": 0,
+    "query": {
+        "range" : {
+            "timestamp_s" : {
+                "gte" : req.params.startTime,
+                "lte" : req.params.endTime
+            }
+        }
+    }
+  }
+  }).then(function (resp) {
+    
+    var hits = resp.hits.hits;
+    console.log('hits length : ',hits.length)
+    //var hits = resp;
+
+    var result = []
+
+    //var obj = hits[0]._source[qtype]  
+    if(topListQType == 'countQtype'){
+      var obj = {"A":0,"NS":0,"CNAME":0,"SOA":0,"WKS":0,"PTR":0,"MX":0,"SRV":0,"AAAA":0,"ANY":0}
+      for(var i=0 ;i<hits.length;i++){
+        obj.A += hits[i]._source[topListQType].A
+        obj.NS += hits[i]._source[topListQType].NS
+        obj.CNAME += hits[i]._source[topListQType].CNAME
+        obj.SOA += hits[i]._source[topListQType].SOA
+        obj.WKS += hits[i]._source[topListQType].WKS
+        obj.PTR += hits[i]._source[topListQType].PTR
+        obj.MX += hits[i]._source[topListQType].MX
+        obj.SRV += hits[i]._source[topListQType].SRV
+        obj.AAAA += hits[i]._source[topListQType].AAAA
+        obj.ANY += hits[i]._source[topListQType].ANY
+      }
+    }
+    else if(topListQType == 'countOpcode'){
+      var obj = {"AA":0,"TC":0,"RD":0,"RA":0,"CD":0,"AD":0,"QR":0}
+      for(var i=0 ;i<hits.length;i++){
+        obj.AA += hits[i]._source[topListQType].AA
+        obj.TC += hits[i]._source[topListQType].TC
+        obj.RD += hits[i]._source[topListQType].RD
+        obj.RA += hits[i]._source[topListQType].RA
+        obj.CD += hits[i]._source[topListQType].CD
+        obj.AD += hits[i]._source[topListQType].AD
+        obj.QR += hits[i]._source[topListQType].QR
+      }
+    }
+
+//brute force add section
+    else if(topListQType == 'countQuery'){
+      var obj = []
+      
+      for(var i=0 ;i<hits.length;i++){
+        console.log(i)
+        var obj2 = hits[i]._source.countQuery
+        for (var j = 0; j < obj2.length; j++){
+          nameExist = false
+          for(var k = 0;k < obj.length; k++){
+            if (obj[k].hostname == obj2[j].hostname){
+              nameExist = true
+              obj[k].count += obj2[j].count;
+              break;
+            }
+          }
+          if(!nameExist){
+            obj.push(obj2[j])
+          }
+        }
+      }
+    }
+    else if(topListQType == 'countIpsource'){
+      var obj = []
+      var nameExist = false
+      for(var i=0 ;i<hits.length;i++){ 
+        console.log(i)
+        var obj2 = hits[i]._source.countIpsource
+        for (var j = 0; j < obj2.length; j++){
+          nameExist = false
+          for(var k = 0;k < obj.length; k++){
+            if (obj[k].hostname == obj2[j].hostname){
+              nameExist = true
+              obj[k].count += obj2[j].count;
+              break;
+            }
+          }
+          if(!nameExist){
+            obj.push(obj2[j])
+          }
+        }
+      }
+    }
+
+/*    for(a in obj){
+      result.push([a,obj[a]])
+    }*/
+
+//sort
+/*    result.sort(function(a,b){return a[1] - b[1]});
+    result.reverse();  
+
+    var returnRes = {}
+    returnRes.data = result*/
+
+    //res.json(hits);
+    //res.json(returnRes);
+    res.json(obj)
+  }, function (err) {
+      console.trace(err.message);
+      console.log(err.message)
+  });
+  //next()
+})
+
+router.get('/topList2/:startTime/:endTime', function (req, res, next) {
   console.log('startTime:', req.params.startTime);
   console.log('endTime:', req.params.endTime);
 
   console.log('qtype:', req.query.qtype)
+  var topListQType2 = req.query.qtype;
 
-  //var days = 1
-  //var querySize = 1000
   client.search({
-  index: 'dnsanalyzerstore',
+  index: 'dnsanalyzerstore3',
   type: 'analyzedhour',
   body: {
     "size": 1000,
@@ -150,6 +286,7 @@ router.get('/topList/:startTime/:endTime', function (req, res, next) {
 /*    "sort" : [
         { "countQtype.A" : { "order" : "asc"}}
     ],*/
+    //"_source": ["countIpsource"],
     "query": {
         "range" : {
             "timestamp_s" : {
@@ -158,69 +295,35 @@ router.get('/topList/:startTime/:endTime', function (req, res, next) {
             }
         }
     },
-/*    aggs: {
-        "sum_result" : { "sum" : { "field" : "countOpcode.AA" } }
-    }*/
-
-//you must enable field_data before use this section below
-/*    "aggs" : {
-        "sum_result" : {
+    "aggs" : {
+          "Nest" : {
             "nested" : {
-                "path" : "countIpsource"
+                "path" : topListQType2
             },
             "aggs" : {
-                "countIpsource1234" : { "sum" : { "field" : "countIpsource.hostname" } }
+                "Grouping": {
+                  "terms": {
+                    "field": topListQType2+".hostname",
+                    "size":100,
+                    "order": {"sumcount":"desc"}
+                  },
+                  "aggs": {
+                    "sumcount": {
+                      "sum": {"field": topListQType2+".count"},
+                    }
+                  }
+                }
             }
         }
-    }*/
-
+    }
   }
   }).then(function (resp) {
-    var qtype = req.query.qtype;
-    var hits = resp.hits.hits;
-    //var hits = resp.aggregations;
+    //var hits = resp.hits.hits;
+    var hits = resp.aggregations;
     //var hits = resp;
 
-    var result = []
-
-    var obj = hits[0]._source[qtype]
-    //var obj = hits[0]._source.countOpcode
-
-    //under construction
-    //var obj = hits[0]._source.countQuery
-    //var obj = hits[0]._source.countIpsource
-
-//type selector(not complete)
-/*    var resType = countQtype
-    if(req.query.type == 'qtype'){
-
-    }*/
-
-//result aggragate(not complete)
-/*    for(var i=0;i<hits.length;i++){
-      result[i] = hits[i]._source.countQtype
-    }*/
-
-//type2
-/*    for(a in obj){
-      result.push([obj[a].hostname,obj[a].count])
-    }*/
-//type1
-
-    for(a in obj){
-      result.push([a,obj[a]])
-    }
-
-
-//sort
-    result.sort(function(a,b){return a[1] - b[1]});
-    result.reverse();  
-
-    var returnRes = {}
-    returnRes.data = result
-
-    //res.json(hits);
-    res.json(returnRes);
+    res.json(hits);
+    //res.json(returnRes);
     //res.send('test');
   }, function (err) {
       console.trace(err.message);
